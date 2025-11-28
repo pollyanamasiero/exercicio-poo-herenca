be746d04f247c350cd90a4b0ac97136bcbf9c45e
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-11-27 21:23:31 -0300

    código da aula de POO - herança e polimorfismo completo

--- heranca.py
+++ heranca.py
@@ -1,11 +1,18 @@
 class Animal:
-    def __init__(self, nome):
+    def __init__(self, nome, peso):
         self.nome = nome
+        self.peso = peso
+        
+    def descrever(self):
+        print(f'[ANIMAL] Nome: {self.nome} | Peso: {self.peso}kg.')
         
     def emitir_som(self):
-        print(f'{self.nome} fez um som genérico')
+        print(f'{self.nome} fez um som genérico.')
         
 class Cachorro(Animal):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+        
     def emitir_som(self):
         print(f'{self.nome} está latindo: au au!')
 
@@ -18,24 +25,30 @@ class Vaca(Animal):
         print(f'{self.nome} está mugindo: muu!')
         
 class Ave(Animal):
-    def __init__(self, nome, envergadura):
-        super().__init__(nome) #pega o nome do animal
+    def __init__(self, nome, peso, envergadura):
+        super().__init__(nome, peso) #pega o nome do animal
         self.envergadura = envergadura
+        
+    def descrever(self):
+        super().descrever()
+        print(f'Envergadura: {self.envergadura}m.')
     
     def emitir_som(self):
         #chamando o método da classe pai
         super().emitir_som() #opcional
         print(f'{self.nome} está cantando: fiuuu!')
-        print(f'{self.nome} tem {self.envergadura}m de envergadura.')
 
 
 if __name__ == '__main__':
     animais = [
-        Cachorro('Spike'),
-        Gato('Khaleesi'),
-        Vaca('Mimosa'),
-        Ave('Loro', 1.98)
+        Cachorro('Spike', 25),
+        # Gato('Khaleesi'),
+        # Vaca('Mimosa'),
+        Ave('Loro', 3, 1.98)
     ]
     
     for animal in animais:
+        print('-' * 40)
+        animal.descrever()
         animal.emitir_som()
+        print('-' * 40)
