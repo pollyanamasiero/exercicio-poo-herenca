b448602e17993170794c63c82a9a3ee89fa46942
pollyanamasiero <pollyanamasiero@hotmail.com>
2025-11-27 21:12:51 -0300

    usando super()

--- heranca.py
+++ heranca.py
@@ -16,13 +16,25 @@ class Gato(Animal):
 class Vaca(Animal):
     def emitir_som(self):
         print(f'{self.nome} está mugindo: muu!')
+        
+class Ave(Animal):
+    def __init__(self, nome, envergadura):
+        super().__init__(nome) #pega o nome do animal
+        self.envergadura = envergadura
+    
+    def emitir_som(self):
+        #chamando o método da classe pai
+        super().emitir_som() #opcional
+        print(f'{self.nome} está cantando: fiuuu!')
+        print(f'{self.nome} tem {self.envergadura}m de envergadura.')
 
 
 if __name__ == '__main__':
     animais = [
         Cachorro('Spike'),
         Gato('Khaleesi'),
-        Vaca('Mimosa')
+        Vaca('Mimosa'),
+        Ave('Loro', 1.98)
     ]
     
     for animal in animais:
